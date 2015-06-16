112553a9e0c342fa9a1bd0e3328cc03f19ebd2e7
Andrew Petersen <kirbysayshi@gmail.com>
2015-06-16 05:10:59 -0400

    rewrote the whole thing, but in level-schedule held me up forever
    https://github.com/juliangruber/level-schedule/pull/6
    
    still need to actually generate the real prompts. so close though.

--- webhook.js
+++ webhook.js
@@ -1,22 +1,16 @@
 var conf = require('./lib/conf');
-var twilio = require('./lib/twilio')(conf.get('twilioSID'), conf.get('twilioTOKEN'));
+
 var http = require('http');
 var concat = require('concat-stream');
 var querystring = require('querystring');
 
-var usersDB = require('./lib/db').register('users');
-var promptsDB = require('./lib/db').register('prompts');
-var jobsDB = require('level-schedule')(require('./lib/db').register('jobs'));
+var User = require('./lib/user');
+var Prompt = require('./lib/prompt');
+var DialogState = require('./lib/dialog-state');
 
-var PROMPT_JOB_KEY = 'promptjob';
-jobsDB.job(PROMPT_JOB_KEY, function(payload, done) {
-  console.log('JOB RUN GO!', payload);
-  done(null);
-});
-
-// TODO: somehow break this twilio dialog tree into something comprehensible
-
-// TODO: use a uuid instead of the user's phone number as an ID
+// Connect the job scheduler to the job immediately, since it needs to be
+// defined before attempting to resume pending jobs.
+Prompt.setJobHook(DialogState.onShouldSendPrompt);
 
 http.createServer(function(req, res) {
 
@@ -36,58 +30,15 @@ http.createServer(function(req, res) {
     console.log('webhook body parsed', parsed);
     console.log('FROM number', parsed.From);
 
-    getUser(parsed.From, function(err, userInfo) {
-      if (!err) {
-        console.log('found user', userInfo);
-        // user exists, check if this is a verification response
-        var normaled = parsed.Body.trim().toLowerCase()
-
-        if (userInfo.verified === false) {
-          if (normaled === 'yes'
-            || normaled === 'yeah'
-            || normaled === 'ok'
-            || normaled === 'go'
-            || normaled === 'awesome'
-            || normaled === 'radical'
-            || normaled === 'woo'
-            || normaled === 'wooo'
-            || normaled === 'cool'
-            || normaled === 'coo'
-          ) {
-            verifyUser(userInfo, function(err) {
-              console.log('user verified if null', err);
-              // TODO: queue user to receive prompts!
-              if (err) return twimlResponse(res, 200, 'Something went wrong saving your user info.');
-              else {
-                jobsDB.run(PROMPT_JOB_KEY, {userInfo: userInfo}, Date.now() + 10000);
-                return twimlResponse(res, 200, 'Thanks for joining! Expect a prompt within a bit.');
-              }
-            })
-          } else {
-            console.log('bad verification response');
-            twimlResponse(res, 200, 'Sorry, I didn\'t understand that. Could you reply with either a YES or just ignore me?');
-          }
-          return;
-        }
-
-        // grab last question asked, store answer
-        getLatestUnansweredPrompt(parsed.From, function(err, latest) {
-          if (err || !latest) return twimlResponse(res, 200, 'Not sure what you mean, I didn\'t ask you a question yet.');
-
-          console.log('found latest prompt', latest);
-          latest.value.answer = parsed.Body;
-          savePrompt(parsed.From, latest.key, latest.value, function(err) {
-            if (err) return twimlResponse(res, 200, 'Failed to save prompt response, try again.');
-            else twimlResponse(res, 200, 'Thanks.');
-          })
-        })
+    User.getOrCreateFromPhoneNumber(parsed.From, function(err, userInfo) {
+      if (err) {
+        console.log(err);
         return;
       }
-
-      // New user, create
-      sendCreateVerification(parsed.From, function(err, userInfo) {
-        if (err) return twimlResponse(res, 200, 'Sorry, something went wrong creating your account.');
-      })
+      DialogState.handle(userInfo, parsed.Body, function(err, message) {
+        if (err) return twimlResponse(res, 200, err.message);
+        if (message) twimlResponse(res, 200, message);
+      });
     });
   }))
 
@@ -106,73 +57,4 @@ function twimlResponse(res, code, msg) {
 
   res.write(xml);
   res.end();
-}
-
-function getUser(from, next) {
-  usersDB.get('user!' + from, function(err, user) {
-
-    if (!err) {
-      return next(err, user);
-    }
-
-    return next(err);
-  });
-}
-
-function verifyUser(userInfo, next) {
-  userInfo.verified = true;
-  usersDB.put('user!' + userInfo.number, userInfo, function(err) {
-    return next(err);
-  })
-}
-
-function sendCreateVerification(from, next) {
-  var userInfo = {
-    number: from,
-    joined: Date.now(),
-    verified: false
-  }
-
-  usersDB.put('user!' + from, userInfo, function(err) {
-    if (err) {
-      return next(err);
-    }
-
-    twilio.sendMessage({
-      to: from,
-      from: conf.get('twilioNumber'),
-      body: ''
-        + 'Welcome to Question-Book! \n'
-        + 'If you\'d like to join, send back something positive, like "YES".\n'
-        + 'If not, ignore!\n'
-        + 'For more info, visit ' + conf.get('publicAddress')
-    }, function(err, res) {
-      if (err) {
-        console.error(err);
-        return next(err);
-      }
-      return next(null, userInfo);
-    });
-  });
-}
-
-function getLatestUnansweredPrompt(from, next) {
-  var prompts = promptsDB.createReadStream({
-    gte: 'prompts!' + from + '!' + (Date.now() - conf.get('maxTimeBetweenPrompts'))
-  });
-
-  prompts.pipe(concat(function(all) {
-    var latest = all.pop();
-    if (!latest) return next(null, null);
-    if (latest.value.answer !== null) return next(null, null);
-    return next(null, latest);
-  }));
-}
-
-function savePrompt(from, prompt, key, next) {
-  prompt.answer = prompt.answer || null
-  key = key || 'prompts!' + from + '!' + Date.now();
-  promptsDB.put(key, prompt, function(err) {
-    return next(err);
-  });
 }
